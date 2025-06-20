e52a375ce49debcd087ad8810af6824801d8fff5
uptomaster <lnh929@naver.com>
2025-06-20 18:40:14 +0900

    250620 java day08

---
+++ src/review/Stack.java
@@ -0,0 +1,14 @@
+package review;
+
+public class Stack {
+	public static void main(String[] args) {
+		
+		
+		int[] arr1 = new int[] {1,2,3,4,5}; // 처음 만든 배열
+//		arr1 = {5,8,7}; // 이런 방식으로 바꾸는건 허락되지않음. 이미 주소가 만들어져있기 때문
+		arr1 = new int[] {5,8,7}; // 이렇게 바꾸는건 허용됨. 자동으로 힙에서 메모리를 해제하고 할당한다.
+		
+		
+	}
+
+}
